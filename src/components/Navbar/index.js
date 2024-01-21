8e6b7528095d525067f5edb1197a4aa77cf1c105
Zyrephus <wilsonchen315@gmail.com>
2024-01-21 12:56:53 -0500

    made icons and navbar from mui

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,28 +1,59 @@
 import React from "react";
-import { Nav, NavLink, NavMenu } from "./NavbarElements";
+import { Link } from 'react-router-dom';
+
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import EmailIcon from '@mui/icons-material/Email';
+import BookIcon from '@mui/icons-material/Book';
+import LoginIcon from '@mui/icons-material/Login';
  
 const Navbar = () => {
+    const [value, setValue] = React.useState('recents');
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
         <>
-            <Nav>
-                <NavMenu>
-                    <NavLink to="./" activeStyle>
-                        Home
-                    </NavLink>
-                    <NavLink to="/about" activeStyle>
-                        About
-                    </NavLink>
-                    <NavLink to="/contact" activeStyle>
-                        Contact Us
-                    </NavLink>
-                    <NavLink to="/blogs" activeStyle>
-                        Blogs
-                    </NavLink>
-                    <NavLink to="/sign-up" activeStyle>
-                        Sign Up
-                    </NavLink>
-                </NavMenu>
-            </Nav>
+            <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
+                <BottomNavigationAction
+                    label="Home"
+                    value="home"
+                    icon={<HomeIcon />}
+                    component={Link}
+                    to="./"
+                />
+                <BottomNavigationAction
+                    label="About"
+                    value="about"
+                    icon={<InfoIcon />}
+                    component={Link}
+                    to="/about"
+                />
+                <BottomNavigationAction
+                    label="Contact"
+                    value="contact"
+                    icon={<EmailIcon />}
+                    component={Link}
+                    to="/contact"
+                />
+                <BottomNavigationAction
+                    label="Blogs"
+                    value="blogs"
+                    icon={<BookIcon />}
+                    component={Link}
+                    to="/blogs"
+                />
+                <BottomNavigationAction
+                    label="Login"
+                    value="login"
+                    icon={<LoginIcon />}
+                    component={Link}
+                    to="/sign-up"
+                />
+            </BottomNavigation>
         </>
     );
 };
