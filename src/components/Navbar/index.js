5662353187ad78938fa329aaec00eb16dace7e75
Zyrephus <wilsonchen315@gmail.com>
2024-01-28 12:42:11 -0500

    Updated navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import '../../App.css';
+import '../../navbar.css';
  
-const Navbar = () => {
+function Navbar(props) {
     return (
-        <nav className="navbar">
+        <nav className="nav">
             <Link to="./" className="home-link">Wilson's Blog</Link>
-            <Link to="./blogs" className="main-links">Blogs</Link>
-            <Link to="/about" className="main-links">About</Link>
-            <Link to="/contact" className="main-links">Contact</Link>
-            <Link to="/login" className="main-links">Login</Link>
+            <ul>
+                <li><Link to="./blogs" className="main-links">Blogs</Link></li>
+                <li><Link to="/about" className="main-links">About</Link></li>
+                <li><Link to="/contact" className="main-links">Contact</Link></li>
+                <li><Link to="/login" className="main-links">Login</Link></li>
+            </ul>
         </nav>
     );
-};
+}
  
 export default Navbar;
