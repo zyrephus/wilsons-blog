dec8bcbd843668db7f34fbe8a177e2b395e5dd82
Zyrephus <wilsonchen315@gmail.com>
2024-01-29 09:22:41 -0500

    Made login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -13,31 +13,31 @@ const Login = () => {
 
     return (
         <div className="login-container">
-        <h1>Login Page</h1>
-        <form onSubmit={handleSubmit}>
-            <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <input
-                    type="text"
-                    id="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </div>
-            <button type="submit">Login</button>
-        </form>
-    </div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="username">Username</label>
+                    <input
+                        required
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        required
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button>Login</button>
+            </form>
+        </div>
     );
 }
  
