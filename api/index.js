711fd316e0843b7c0057dbc3324354f494b7bb24
Zyrephus <wilsonchen315@gmail.com>
2024-01-31 19:56:57 -0500

    Tokens

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const jwt = require('jsonwebtoken');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfjp9awo4tj9348qjfapsiuo-';
 
-app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://admin:%25BlackDragon73@cluster0.thlymdf.mongodb.net/?retryWrites=true&w=majority');
@@ -37,7 +37,7 @@ app.post('/login', async (req, res) => {
         // Logged in
         jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
             if(err) throw err;
-            res.json(token);
+            res.cookie('token', token).json('ok');
         });
     }
     else {
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -11,6 +11,7 @@ const Login = () => {
             method: 'POST',
             body: JSON.stringify({username, password}),
             headers: {'Content-Type':'application/json'},
+            credentials: 'include',
         })
     };
 
