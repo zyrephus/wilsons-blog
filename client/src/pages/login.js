9e2a96a0af5f3827e7198dc48d822c9a0f7863ff
Zyrephus <wilsonchen315@gmail.com>
2024-01-31 20:07:23 -0500

    Backend for authentication semi-done

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,20 +1,30 @@
 import React, { useState } from "react";
+import {Navigate} from "react-router-dom";
 import '../App.css';
  
 const Login = () => {
     const [username, setUsername] = useState(''); // State for username
     const [password, setPassword] = useState(''); // State for password
+    const [redirect, setRedirect] = useState(false);
     
     async function login(event) {
         event.preventDefault();
-        await fetch('http://localhost:4000/login', {
+        const respone = await fetch('http://localhost:4000/login', {
             method: 'POST',
             body: JSON.stringify({username, password}),
             headers: {'Content-Type':'application/json'},
             credentials: 'include',
-        })
-    };
-
+        });
+        if(respone.ok) {
+            setRedirect(true);
+        }
+        else {
+            alert("Wrong credentials.");
+        }
+    }
+    if (redirect) {
+        return <Navigate to={'/'} />
+    }
     return (
         <div className="login-container">
             <h1>Login</h1>
